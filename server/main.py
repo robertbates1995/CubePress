fcfd76e6779cb2a103cb0e7de775441685bf7976
robertbates1995 <thegreatbobini@gmail.com>
2023-01-27 14:51:49 -0800

    separated servos into its own file

--- server/main.py
+++ server/main.py
@@ -1,101 +1,25 @@
 from machine import Pin, PWM
 from utime import sleep_ms
-import sys, select, network, socket, secrets, time
-
-
-class arm_servo:
-    servo = PWM(Pin(15))
-    # arm servo, connected to Pin 15
-    bot = int
-    mid = int
-    top = int
-    
-    def test(self):
-        self.move_top()
-        print("top = " + str(self.top))
-        sleep_ms(1200)
-        self.move_bot()
-        print("bottom = " + str(self.bot))
-        sleep_ms(1200)
-        self.move_mid()
-        print("middle = " + str(self.mid))
-        sleep_ms(1200)
-    
-    def move_bot(self):
-        self.servo.duty_ns(self.bot)
-        sleep_ms(50)
-        
-    def move_mid(self):
-        self.servo.duty_ns(self.mid)
-        sleep_ms(50)
-        
-    def move_top(self):
-        self.servo.duty_ns(self.top)
-        sleep_ms(50)
-        
-    def __init__(self, a, b, c):
-        self.servo.freq(50) 
-        sleep_ms(50)
-        self.bot = a
-        self.mid = b
-        self.top = c
-        
-class table_servo:
-    servo = PWM(Pin(16))
-    # arm servo, connected to Pin 16
-    left = int
-    mid = int
-    right = int
-    
-    def test(self):
-        self.move_left()
-        print("left = " + str(self.left))
-        sleep_ms(1200)
-        self.move_right()
-        print("right = " + str(self.right))
-        sleep_ms(1200)
-        self.move_mid()
-        print("middle = " + str(self.mid))
-        sleep_ms(1200)
-    
-    def move_left(self):
-        self.servo.duty_ns(self.left)
-        sleep_ms(50)
-        
-    def move_mid(self):
-        self.servo.duty_ns(self.mid)
-        sleep_ms(50)
-        
-    def move_right(self):
-        self.servo.duty_ns(self.right)
-        sleep_ms(50)
-        
-    def __init__(self, a, b, c):
-        self.servo.freq(50)
-        sleep_ms(50)
-        self.left = a
-        self.mid = b
-        self.right = c
-
-
-
-#def main_func():
-    
+import sys, select, network, socket, secrets, time, servos
     
 ############################################# MAIN PROGRAM ####################################################
 
+#initalize servos and LED
 led = Pin("LED", Pin.OUT)
 ledState = 'LED State Unknown'
+arm = servos.arm_servo(1360000,1230000,750000, 15)
+table = servos.table_servo(3000000,1550000,750000, 16)
 
-button = Pin(16, Pin.IN, Pin.PULL_UP)
-
+#set secret values
 ssid = secrets.ssid
 password = secrets.password
 
+#connect to local network
 wlan = network.WLAN(network.STA_IF)
 wlan.active(True)
 wlan.connect(ssid, password)
-    
+
+#webpage html
 html = """<!DOCTYPE html>
 <html>
 <head> <title>Pico W</title> </head>
@@ -123,19 +47,12 @@ else:
     status = wlan.ifconfig()
     print( 'ip = ' + status[0] )
     
-    
 # Open socket
 addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
 s = socket.socket()
 s.bind(addr)
 s.listen(1)
 print('listening on', addr)
-
-#initalize servos
-arm = arm_servo(1360000,1230000,750000)
-#initalize arm servo
-table = table_servo(3000000,1550000,750000)
-#initalize table servo
 
 # Listen for connections, serve client
 while True:
@@ -171,16 +88,8 @@ while True:
         ledState = "LED is OFF" if led.value() == 0 else "LED is ON" # a compact if-else statement
         #armState = "Arm is OFF" if arm.value() == 0 else "Arm is ON"
         
-        if button.value() == 1: # button not pressed
-            print("button NOT pressed")
-            buttonState = "Button is NOT pressed"
-        else:
-            print("button pressed")
-            buttonState = "Button is pressed"
-        
         # Create and send response
-        stateis = ledState + " and " + buttonState
-        response = html % stateis
+        response = html % ledState
         cl.send('HTTP/1.0 200 OK\r\nContent-type: text/html\r\n\r\n')
         cl.send(response)
         cl.close()
