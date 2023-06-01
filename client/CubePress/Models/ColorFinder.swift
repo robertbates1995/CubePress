e559ddade67835047e3725fc797101b56073a35d
Robert Bates <37717366+robertbates1995@users.noreply.github.com>
2023-06-01 10:23:58 -0700

    changed teststrip size, extended UIimage

--- client/CubePress/Models/ColorFinder.swift
+++ client/CubePress/Models/ColorFinder.swift
@@ -19,47 +19,7 @@ class ColorFinder {
         return imageClassifierVisionModel
     }()
     
-    func createTestStrip(base: UIImage, centers: [UIImage]) -> UIImage {
-        let size = CGSize(width: 300, height: 400)
-        
-        UIGraphicsBeginImageContext(size)
-        UIColor.white.setFill()
-        UIRectFill(CGRect(origin: .zero, size: size))
-        
-        //add u face
-        let uSquare = CGRect(x: 100, y: 0, width: 100, height: 100)
-        base.draw(in: uSquare)
-        centers[0].draw(in: uSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add l face
-        let lSquare = CGRect(x: 0, y: 100, width: 100, height: 100)
-        base.draw(in: lSquare)
-        centers[1].draw(in: lSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add f face
-        let fSquare = CGRect(x: 100, y: 100, width: 100, height: 100)
-        base.draw(in: fSquare)
-        centers[2].draw(in: fSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add r face
-        let rSquare = CGRect(x: 200, y: 100, width: 100, height: 100)
-        base.draw(in: rSquare)
-        centers[3].draw(in: rSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add b face
-        let bSquare = CGRect(x: 100, y: 300, width: 100, height: 100)
-        base.draw(in: bSquare)
-        centers[4].draw(in: bSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add d face
-        let dSquare = CGRect(x: 100, y: 200, width: 100, height: 100)
-        base.draw(in: dSquare)
-        centers[5].draw(in: dSquare, blendMode: .difference, alpha: 1.0)
-        
-        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return newImage
-    }
+    
     
     func calcColor(image: UIImage, base: [UIImage]) -> UIColor {
         if base.count < 1 {
@@ -74,7 +34,7 @@ class ColorFinder {
         
         imageClassificationRequest.imageCropAndScaleOption = .scaleFill
         
-        guard let image = CIImage(image: createTestStrip(base: image, centers: base))
+        guard let image = CIImage(image: image.createTestStrip(with: base))
         else { return .black }
         
         let handler = VNImageRequestHandler(ciImage: image)
--- client/CubePress/Models/CubeMapModel.swift
+++ client/CubePress/Models/CubeMapModel.swift
@@ -83,56 +83,10 @@ class CubeMapModel: ObservableObject {
         saveFacePicturesFrom(D, "D")
     }
     
-    func createTestStrip(base: UIImage, centers: [UIImage]) -> UIImage {
-        let size = CGSize(width: 300, height: 400)
-        
-        UIGraphicsBeginImageContext(size)
-        UIColor.white.setFill()
-        UIRectFill(CGRect(origin: .zero, size: size))
-        
-        //add u face
-        let uSquare = CGRect(x: 100, y: 0, width: 100, height: 100)
-        base.draw(in: uSquare)
-        centers[0].draw(in: uSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add l face
-        let lSquare = CGRect(x: 0, y: 100, width: 100, height: 100)
-        base.draw(in: lSquare)
-        centers[1].draw(in: lSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add f face
-        let fSquare = CGRect(x: 100, y: 100, width: 100, height: 100)
-        base.draw(in: fSquare)
-        centers[2].draw(in: fSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add r face
-        let rSquare = CGRect(x: 200, y: 100, width: 100, height: 100)
-        base.draw(in: rSquare)
-        centers[3].draw(in: rSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add b face
-        let bSquare = CGRect(x: 100, y: 300, width: 100, height: 100)
-        base.draw(in: bSquare)
-        centers[4].draw(in: bSquare, blendMode: .difference, alpha: 1.0)
-        
-        //add d face
-        let dSquare = CGRect(x: 100, y: 200, width: 100, height: 100)
-        base.draw(in: dSquare)
-        centers[5].draw(in: dSquare, blendMode: .difference, alpha: 1.0)
-        
-        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
-        UIGraphicsEndImageContext()
-        return newImage
-    }
-    
     fileprivate func saveFaceTestStrips(_ face: Face, _ named: String) {
         var foo = 0
-        let date = Int64(Date().timeIntervalSince1970)
-        var strips: [UIImage] = []
-        
-        for i in face.sourceImages {
-            strips += [createTestStrip(base: i, centers: centers)]
-        }
+        let date = Int64(Date().timeIntervalSince1970 * 10000)
+        var strips: [UIImage] = face.sourceImages.map({$0.createTestStrip(with: centers)})
         
         let directory = URL.documentsDirectory.appendingPathComponent(named)
         try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true)
@@ -248,4 +202,48 @@ enum FaceSquare: String, CaseIterable, Identifiable, Codable {
     case topLeft, topCenter, topRight,
          midLeft, midCenter, midRight,
          bottomLeft, bottomCenter, bottomRight
+}
+
+extension UIImage {
+    func createTestStrip(with centers: [UIImage]) -> UIImage {
+        let size = CGRect(x: 0, y: 0, width: 128/3, height: 128/4)
+        
+        UIGraphicsBeginImageContext(CGSize(width: 128, height: 128))
+        UIColor.white.setFill()
+        UIRectFill(CGRect(x: 0, y: 0, width: 128, height: 128))
+        
+        //add u face
+        let uSquare = size.offsetBy(dx: size.width, dy: 0)
+        draw(in: uSquare)
+        centers[0].draw(in: uSquare, blendMode: .difference, alpha: 1.0)
+        
+        //add l face
+        let lSquare = size.offsetBy(dx: 0, dy: size.height)
+        draw(in: lSquare)
+        centers[1].draw(in: lSquare, blendMode: .difference, alpha: 1.0)
+        
+        //add f face
+        let fSquare = size.offsetBy(dx: size.width, dy: size.height)
+        draw(in: fSquare)
+        centers[2].draw(in: fSquare, blendMode: .difference, alpha: 1.0)
+        
+        //add r face
+        let rSquare = size.offsetBy(dx: size.width * 2, dy: size.height)
+        draw(in: rSquare)
+        centers[3].draw(in: rSquare, blendMode: .difference, alpha: 1.0)
+        
+        //add b face
+        let bSquare = size.offsetBy(dx: size.width, dy: size.height * 3)
+        draw(in: bSquare)
+        centers[4].draw(in: bSquare, blendMode: .difference, alpha: 1.0)
+        
+        //add d face
+        let dSquare = size.offsetBy(dx: size.width, dy: size.height * 2)
+        draw(in: dSquare)
+        centers[5].draw(in: dSquare, blendMode: .difference, alpha: 1.0)
+        
+        let newImage:UIImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+        return newImage
+    }
 }
